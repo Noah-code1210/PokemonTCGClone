30559e78d5cea933da0cda9f6d1e8270e36eba8d
Noah <noahjames1035@gmail.com>
2025-10-07 09:47:59 -0400

    header left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Logo from "../../assets/Logo.png";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { FaRegCircle } from "react-icons/fa";
 import Mega from "../../assets/mega.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 export default function Home() {
   return (
@@ -48,19 +49,23 @@ export default function Home() {
           </li>
         </ul>
       </nav>
-      <header className="bg-[url('../../assets/Lucario.jpg')] bg-cover bg-center bg-no-repeat w-full max-w-[1470px] mt-21">
-        <div className="w-full max-w-[1470px] flex justify-start px-10 h-items-center">
-          <div className="flex p-2">
-            <div className="w-full h-[660px] items-center justify-center">
-              <Image src={Mega} width={100} height={100} alt="mega" />
-            </div>
-          </div>
-          <div className="w-full flex justify-end pt-2">
-            <button className="w-full max-w-[160px] h-[48px] rounded-[3px] gap-x-2 right-2 bg-black flex items-center justify-center text-white py-[10px] px-3 cursor-pointer">
-              Reduce motion
-              <FaRegCircle className="text-[#e6e6e6]" />
+      <header className="bg-[url('../../assets/Lucario.jpg')] absolute bg-cover bg-center bg-no-repeat w-full max-w-[1470px] h-[654px] mt-21">
+        <div className="w-full max-w-[1470px] flex py-48 px-10">
+          <div className="w-full flex flex-col align-center pt-2 gap-y-7 max-w-[420px] items-center ">
+            <Image src={Mega} width={430} height={200} alt="mega" />
+            <button className="btn from-[#6ee7ff] to-[#37ffbc] max-w-[380px] hover:from-[#ffef14] hover:to-[#fbb064] hover:drop-shadow-[#ff8863]">
+              <span className="pr-2">Go to expansion page</span>
+              <MdOutlineKeyboardArrowRight className="text-3xl" />
+            </button>
+            <button className="btn from-[#ffef14] to-[#fbb064] max-w-[291px] hover:from-[#6ee7ff] hover:to-[#37ffbc] hover:drop-shadow-[#019fe6]">
+              <span className="pr-2">See card gallery</span>
+              <MdOutlineKeyboardArrowRight className="text-3xl" />
             </button>
           </div>
+          <button className="w-full max-w-[160px] h-[48px] rounded-[3px] gap-x-2 bg-black flex items-center justify-center text-white py-[10px] px-3 cursor-pointer absolute top-6 right-2">
+            Reduce motion
+            <FaRegCircle className="text-[#e6e6e6]" />
+          </button>
         </div>
       </header>
     </div>
